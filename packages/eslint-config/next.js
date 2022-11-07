f384bedb7e36f46fc36becb02e38fdf790ecf3c0
Lukas Skamarakas <lukas.skamarakas@kilo.health>
2022-11-07 13:10:03 +0200

    fix: update NextJs rules

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,6 +1,8 @@
+const ERROR = 2;
+
 module.exports = {
   extends: [require.resolve('./react.js'), 'plugin:@next/next/recommended'],
   rules: {
-    'no-underscore-dangle': [error, { allow: ['__NEXT_DATA__'] }],
+    'no-underscore-dangle': [ERROR, { allow: ['__NEXT_DATA__'] }],
   },
 };
